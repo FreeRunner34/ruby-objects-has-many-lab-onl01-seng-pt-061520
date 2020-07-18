2d2f52e783af97250a3b1143f7f471a354fb081c
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-07-18 21:31:26 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,7 +15,6 @@ class Author
 
   def add_post(post)
     post.author = self
-
   end
 
   def add_post_by_title(post_title)
@@ -24,7 +23,7 @@ class Author
  
   end
 
-  def self.post _count
+  def self.post_count
     Post.all.count
   end
 
